94919736e34436f5c41400980b69ded97a8eb701
Tamas ne <thomasneumer@gmail.com>
2017-07-09 18:52:16 +0200

    Sample1 Test

--- Misc/TestDrivenDevelopment/Samples/Sample1/UnitTests/sample1_unittest.cc
+++ Misc/TestDrivenDevelopment/Samples/Sample1/UnitTests/sample1_unittest.cc
@@ -1,39 +1,19 @@
-#include <limits.h>
 #include "sample1.h"
 #include "gtest/gtest.h"
-
+#include <limits.h>
 
 // The test case name and the test name should both be valid C++
 // identifiers.  And you should not use underscore (_) in the names.
 
 // Tests factorial of negative numbers.
 TEST(FactorialTest, Negative) {
-  // This test is named "Negative", and belongs to the "FactorialTest"
-  // test case.
   EXPECT_EQ(1, Factorial(-5));
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_GT(Factorial(-10), 0);
-
-  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
 }
 
 // Tests factorial of 0.
-TEST(FactorialTest, Zero) {
-  EXPECT_EQ(1, Factorial(0));
-}
+TEST(FactorialTest, Zero) { EXPECT_EQ(1, Factorial(0)); }
 
 // Tests factorial of positive numbers.
 TEST(FactorialTest, Positive) {
@@ -42,9 +22,6 @@ TEST(FactorialTest, Positive) {
   EXPECT_EQ(6, Factorial(3));
   EXPECT_EQ(40320, Factorial(8));
 }
-
-
-// Tests IsPrime()
 
 // Tests negative input.
 TEST(IsPrimeTest, Negative) {
@@ -70,15 +47,3 @@ TEST(IsPrimeTest, Positive) {
   EXPECT_FALSE(IsPrime(6));
   EXPECT_TRUE(IsPrime(23));
 }
-
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
